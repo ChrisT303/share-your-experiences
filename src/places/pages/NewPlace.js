349957e5280031de32e206528c9040d6d3d194ad
deftonechris <deftonechris@msn.com>
2023-01-16 13:48:43 -0700

    reducer in place for validator, moving to actual validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-import PlaceInput from '../../shared/components/FormElements/PlaceInput'
-import './NewPlace.css'
+import PlaceInput from "../../shared/components/FormElements/PlaceInput";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-    return <form className="place-form">
-        <PlaceInput elementToggle='input' type= "text" label="Title"/>
-    </form>;
-}
-export default NewPlace
+  return (
+    <form className="place-form">
+      <PlaceInput
+        elementToggle="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Title invalid. Please check and try again"
+      />
+    </form>
+  );
+};
+export default NewPlace;
--- src/shared/components/FormElements/PlaceInput.js
+++ src/shared/components/FormElements/PlaceInput.js
@@ -1,19 +1,57 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 import "./PlaceInput.css";
 
+const reducerInput = (state, action) => {
+  switch (action.type) {
+    case "inputChange":
+      return {
+        ...state,
+        value: action.inputVal,
+        isValid: true,
+      };
+    default:
+      return state;
+  }
+};
+
 const PlaceInput = (props) => {
+  const [inputState, dispatch] = useReducer(reducerInput, {
+    value: "",
+    isValid: false,
+  });
+
+  const inputChangeHandler = (e) => {
+    dispatch({ type: "inputChange", inputVal: e.target.value });
+  };
+
   const elementToggle =
     props.elementToggle === "input" ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onInput={inputChangeHandler}
+        value={inputState.value}
+      />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3 } />
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onInput={inputChangeHandler}
+        value={inputState.value}
+      />
     );
 
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${
+        !inputState.isValid && "form-control--invalid"
+      }`}
+    >
       <label htmlFor={props.id}>{props.label}</label>
       {elementToggle}
+      {!inputState.isValid && <p>{props.errorText}</p>}
     </div>
   );
 };
