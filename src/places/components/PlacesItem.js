e0dbc6457c2b9a02e256f615c6fa9f2644dd6b2c
deftonechris <deftonechris@msn.com>
2023-01-25 20:42:59 -0700

    delete warning modal operational

--- src/places/components/PlacesItem.js
+++ src/places/components/PlacesItem.js
@@ -3,15 +3,29 @@ import React, { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
-import Map from '../../shared/components/UIElements/Map'
+import Map from "../../shared/components/UIElements/Map";
 import "./PlacesItem.css";
 
 const PlacesItem = (props) => {
   const [showMap, setShowMap] = useState(false);
+  const [showWarningModal, setShowWarningModal] = useState(false);
 
   const openMap = () => setShowMap(true);
 
   const closeMap = () => setShowMap(false);
+
+  const showDeleteHandler = () => {
+    setShowWarningModal(true);
+  };
+
+  const cancelDeleteHandler = () => {
+    setShowWarningModal(false);
+  };
+
+  const deleteConfirmationHandler = () => {
+   setShowWarningModal(false);
+    console.log("Deleted");
+  };
 
   return (
     <>
@@ -20,13 +34,27 @@ const PlacesItem = (props) => {
         onClose={closeMap}
         header={props.address}
         contentClass="place-item__modal-content"
-        footerClass= 'place-item__modal-actions'
+        footerClass="place-item__modal-actions"
         footer={<Button onClick={closeMap}>Close</Button>}
       >
-         <div className="map_container">
-            <Map center={props.coordinates} zoom={16}/>
-         </div>
-         </Modal>
+        <div className="map_container">
+          <Map center={props.coordinates} zoom={16} />
+        </div>
+      </Modal>
+      <Modal
+        show={showWarningModal}
+        onCancel={cancelDeleteHandler}
+        header="Warning!"
+        footerClass="place-item__modal-actions"
+        footer={
+          <>
+            <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
+            <Button danger onClick={deleteConfirmationHandler}>DELETE</Button>
+          </>
+        }
+      >
+        <p>Are you sure you want to permanently delete?</p>
+      </Modal>
       <li className="place-item">
         <Card className="place-item__content">
           <div className="place-item__image">
@@ -38,9 +66,11 @@ const PlacesItem = (props) => {
             <p>{props.description}</p>
           </div>
           <div className="place-item__actions">
-            <Button inverse onClick={openMap}>View On Map</Button>
+            <Button inverse onClick={openMap}>
+              View On Map
+            </Button>
             <Button to={`/places/${props.id}`}>Edit</Button>
-            <Button danger>Delete</Button>
+            <Button danger onClick={showDeleteHandler}>Delete</Button>
           </div>
         </Card>
       </li>
