803f11e02168bc525de3c1fa3842e9585680a9bb
deftonechris <deftonechris@msn.com>
2023-02-05 16:43:04 -0700

    get place by id working

--- server/controllers/places.js
+++ server/controllers/places.js
@@ -1,9 +1,9 @@
-const { v4: uuid } = require("uuid");
+
 const { validationResult } = require("express-validator");
 
-const ErrorHandler = require("../../server/models/error.js");
-const getAddressCoords = require('../../server/util/location.js');
-const Place = require('../../server/models/place.js')
+const ErrorHandler = require("../models/error.js");
+const getAddressCoords = require("../util/location.js");
+const Place = require("../models/place.js");
 
 let fillerPlaces = [
   {
@@ -19,70 +19,78 @@ let fillerPlaces = [
   },
 ];
 
-const getPlaceByID = (req, res) => {
+const getPlaceByID = async (req, res, next) => {
+  const placeIdentifier = req.params.placeID;
+
+  let place;
   try {
-    const placeIdentifier = req.params.placeID;
-    const place = fillerPlaces.find((p) => {
-      return p.id === placeIdentifier;
-    });
-    if (!place) {
-      throw new ErrorHandler("No place found by given id", 404);
-    }
-    res.json({ place });
-  } catch (error) {
-    console.error(error);
-    res.status(error.statusCode || 500).json({
-      message: error.message || "An unknown error occurred!",
-    });
+    place = await Place.findById(placeIdentifier);
+  } catch (err) {
+    const error = new ErrorHandler(
+      "An error occurred, could not find place",
+      500
+    );
+    return next(error);
   }
+
+  if (!place) {
+    const error = new ErrorHandler("No place found by given id", 404);
+    return next(error);
+  }
+  res.json({ place: place.toObject({ getters: true }) });
 };
 
-const getPlacesByUserID = (req, res) => {
+const getPlacesByUserID = async (req, res, next) => {
+  const userID = req.params.uid;
+
+  let places;
   try {
-    const userID = req.params.uuid;
-    const places = fillerPlaces.filter((p) => {
-      return (p.creator = userID);
-    });
-    if (!places || places.length === 0) {
-      throw new ErrorHandler("No place found by given user id", 404);
-    }
-    res.json({ places });
-  } catch (error) {
-    console.error(error);
-    res.status(error.statusCode || 500).json({
-      message: error.message || "An unknown error occurred!",
-    });
+    places = await Place.find({ creator: userID });
+  } catch (err) {
+    const error = new ErrorHandler(
+      "Error finding places, please try again",
+      500
+    );
+    return next(error);
   }
+  if (!places || places.length === 0) {
+    return next(new ErrorHandler("No place found by given user id", 404));
+  }
+  res.json({ places: places.map((p) => p.toObject({ getters: true })) });
 };
 
 const createPost = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return next (new ErrorHandler("Invalid entry", 422));
+    return next(new ErrorHandler("Invalid entry", 422));
   }
 
   const { title, description, address, creator } = req.body;
- 
+
   let coordinates;
   try {
-    
-    coordinates = await getAddressCoords(address)
-  }catch(error){
-   return next(error)
+    coordinates = await getAddressCoords(address);
+  } catch (error) {
+    return next(error);
   }
-  
 
-
-  const createPlace = {
-    id: uuid(),
+  const createPlace = new Place({
     title,
     description,
-    location: coordinates,
     address,
+    location: coordinates,
+    image:
+      "https://www.denverpost.com/wp-content/uploads/2018/06/GettyImages-161361450.jpg?w=620",
     creator,
-  };
-  fillerPlaces.push(createPlace);
+  });
+
+  try {
+    await createPlace.save();
+  } catch (err) {
+    const error = new ErrorHandler("Post failed. Please try again", 500);
+    return next(error);
+  }
 
   res.status(201).json({ place: createPlace });
 };
@@ -107,8 +115,8 @@ const updatePlace = (req, res) => {
 };
 
 const deletePlace = (req, res) => {
-  if(fillerPlaces.find(p => p.id === placeIdentifier )) {
-    throw new ErrorHandler('No place found with this id')
+  if (fillerPlaces.find((p) => p.id === placeIdentifier)) {
+    throw new ErrorHandler("No place found with this id");
   }
   const placeIdentifier = req.params.placeID;
   fillerPlaces = fillerPlaces.filter((p) => p.id !== placeIdentifier);
